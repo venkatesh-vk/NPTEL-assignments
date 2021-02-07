18ec4ee47f41d5eb234ecfcb93857c2a56cf78c4
venkatesh-vk <61019595+venkatesh-vk@users.noreply.github.com>
2021-02-07 15:16:39 +0530

    Update week 2 assignment  prime partion__bracket_check__list_rotate.py

--- week 2 assignment  prime partion__bracket_check__list_rotate.py
+++ week 2 assignment  prime partion__bracket_check__list_rotate.py
@@ -1,21 +1,21 @@
-def primepartition(m):
-    a=[]
-    x=0
-    for j in range(m+1):
-        if j>1:
-            for i in range(2,j):
-                if(j%i==0):
-                    break
-            else:
-                a.append(j)
-    for i in range(len(a)):
-        t=m-a[i]
-        if(t in a):
-                x=1
-                return True
-    if(x==0):
+def isP(n):
+    if n==2:
+        return True
+    if n%2==0:
         return False
-
+    return all(n%x>0 for x in range(3, int(n**0.5)+1, 2))
+ 
+def genP(n):
+    p = [2]
+    p.extend([x for x in range(3, n+1, 2) if isP(x)])
+    return p
+def primepartition(n):
+    p = genP(n)
+    for i in range(0,len(p)):
+        for j in range(0,len(p)):
+            if p[i]+p[j]==n:
+                return True
+    return False
 def matched(s):
     t=0
     x=0
